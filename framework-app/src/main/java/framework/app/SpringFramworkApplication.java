73c6ee3315a509d9cdc77fe2576523e93e77d124
juhyeon <28646684+zoo-hyeon@users.noreply.github.com>
2021-03-10 01:44:45 +0900

    Git 설명 중 커밋 Develop

--- framework-app/src/main/java/framework/app/SpringFramworkApplication.java
+++ framework-app/src/main/java/framework/app/SpringFramworkApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringFramworkApplication {
-
+    // Develop
     public static void main(String[] args) {
         SpringApplication.run(SpringFramworkApplication.class, args);
     }
